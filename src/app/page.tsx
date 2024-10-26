93b49161d1ee5c4462c6180c6988d9cf9c47dc3f
juan <prendas52@gmail.com>
2024-10-25 23:05:47 -0600

    features and footer section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '../components/sections/hero-section'
 import FeatureSection from '../components/sections/feature-section'
-import DashboardSection from '../components/sections/dashboard-section'
 import Section from '../components/sections/section'
 import FooterSection from '../components/sections/footer-section'
 
