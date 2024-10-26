5babc3771231e6724d0ad73519353ef261a829e2
juan <prendas52@gmail.com>
2024-10-26 03:04:11 -0600

    Early Access CTA added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ import FeatureSection from '../components/sections/feature-section'
 import EarlyAccessSection from '../components/sections/early-access-section'
 import FooterSection from '../components/sections/footer-section'
 
-
-
-
-
-
 export default function Home() {
   return (
     <div className="content flex flex-col self-center">
